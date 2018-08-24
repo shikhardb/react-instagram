beb5e0be225e84afc331e01cf0736635b441c1fd
Shikhar Dubey <shikhar.db@gmail.com>
2018-08-24 14:55:33 +0530

    Redirect to home page after authentication

--- insta/src/components/Login.js
+++ insta/src/components/Login.js
@@ -3,14 +3,15 @@ import InstagramLogin from "react-instagram-login";
 
 const ClientId = "f79babbb849c467d9f5dcc02d15ba1e2";
 
-const responseInstagram = response => {
-    console.log(response);
-    if (typeof response) {
-        localStorage.setItem("key", response);
-    }
-};
-
 export default class Login extends Component {
+    responseInstagram = response => {
+        console.log(response);
+        if (typeof response) {
+            localStorage.setItem("access_token", response);
+            this.props.history.push("/home");
+        }
+    };
+
     render() {
         return (
             <div>
@@ -19,9 +20,10 @@ export default class Login extends Component {
                 </h1>
                 <InstagramLogin
                     clientId={ClientId}
-                    onFailure={responseInstagram}
-                    onSuccess={responseInstagram}
+                    onFailure={this.responseInstagram}
+                    onSuccess={this.responseInstagram}
                     implicitAuth="true"
+                    scope="public_content"
                 >
                     <span> Login with Instagram</span>
                 </InstagramLogin>
